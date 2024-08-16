e4d1e412e3f7d286376555c2b3d91e34f7e3f0d5
hongguoqiang <2310087518@qq.com>
2024-08-16 21:54:13 +0800

    fix: update

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,46 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const session = require('express-session');
 
+import * as net from 'net';
+
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class PortService {
+  checkPort(port: number): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      const server = net.createServer();
+
+      server.once('error', (err: any) => {
+        if (err.code === 'EADDRINUSE') {
+          resolve(true); // 端口被占用
+        } else {
+          reject(err);
+        }
+      });
+
+      server.once('listening', () => {
+        server.close();
+        resolve(false); // 端口未被占用
+      });
+
+      server.listen(port);
+    });
+  }
+
+  async findAvailablePort(startPort: number): Promise<number> {
+    let port = startPort;
+    while (await this.checkPort(port)) {
+      port += 1;
+    }
+    return port;
+  }
+}
+
 export default async function bootstrap() {
   console.log('初始化');
+
+  const portUtils = new PortService();
 
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   console.log('初始化完成');
@@ -21,8 +59,8 @@ export default async function bootstrap() {
     }),
   );
   console.log('服务挂载');
-
-  await app.listen(process.env.PORT || 3000);
+  const port = await portUtils.findAvailablePort(3000);
+  await app.listen(port);
   console.log('服务完成');
 }
 bootstrap();
