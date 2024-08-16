d26a8e2fcbc016f2706b3d04422afde9fadaf691
hongguoqiang <2310087518@qq.com>
2024-08-16 15:42:59 +0800

    fix: 增加vercel兼容

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { createServer, proxy } from 'aws-serverless-express';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import * as express from 'express';
+import { createServer, proxy } from 'aws-serverless-express';
 import { Server } from 'http';
 
+const expressApp = express();
 let cachedServer: Server;
 
 async function bootstrapServer(): Promise<Server> {
-  const expressApp = express();
-  const app = await NestFactory.create(AppModule, expressApp);
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp),
+  );
   await app.init();
   return createServer(expressApp);
 }
