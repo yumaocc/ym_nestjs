ca4cc0396608d4640adce7cc9578a6e10eff6312
hongguoqiang <2310087518@qq.com>
2024-08-16 15:11:12 +0800

    fix: 屏蔽数据代码

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { User, UserDocument } from './user.schema';
-import { v4 as uuid } from 'uuid';
-import * as dayjs from 'dayjs';
+// import { InjectModel } from '@nestjs/mongoose';
+// import { Model } from 'mongoose';
+// import { User, UserDocument } from './user.schema';
+// import { v4 as uuid } from 'uuid';
+// import * as dayjs from 'dayjs';
 @Injectable()
 export class UserService {
-  constructor(@InjectModel(User.name) private dp: Model<UserDocument>) {}
+  // @InjectModel(User.name) private dp: Model<UserDocument>
+  constructor() {}
 
-  async get(name: string) {
-    return this.dp.findOne({ name });
-  }
+  // async get(name: string) {
+  //   return this.dp.findOne({ name });
+  // }
 
   async getAll() {
-    const res = (await this.dp.find()) ?? [];
+    const res = [{ name: '小黑', age: '123' }];
     return {
       data: res,
       success: true,
@@ -22,30 +23,30 @@ export class UserService {
     };
   }
 
-  async create(user: User) {
-    const dp = new this.dp({
-      ...user,
-      updateTime: null,
-      id: uuid(),
-    });
-    const res = await dp.save();
-    return res.id;
-  }
+  // async create(user: User) {
+  //   const dp = new this.dp({
+  //     ...user,
+  //     updateTime: null,
+  //     id: uuid(),
+  //   });
+  //   const res = await dp.save();
+  //   return res.id;
+  // }
 
-  async delete(id: User['id']) {
-    await this.dp.findByIdAndDelete(id);
-    return;
-  }
+  // async delete(id: User['id']) {
+  //   await this.dp.findByIdAndDelete(id);
+  //   return;
+  // }
 
-  async update(params: User) {
-    const res = await this.dp.findOneAndUpdate(
-      { id: params.id },
-      {
-        ...params,
-        updateTime: dayjs().valueOf(),
-      },
-    );
+  // async update(params: User) {
+  //   const res = await this.dp.findOneAndUpdate(
+  //     { id: params.id },
+  //     {
+  //       ...params,
+  //       updateTime: dayjs().valueOf(),
+  //     },
+  //   );
 
-    return res.id;
-  }
+  //   return res.id;
+  // }
 }
