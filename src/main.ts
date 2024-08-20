d1366e9fd117f0a2380ed199246bce7f75a0caf8
hongguoqiang <2310087518@qq.com>
2024-08-20 10:50:15 +0800

    feat: 设置监听ip

--- src/main.ts
+++ src/main.ts
@@ -1,66 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const session = require('express-session');
 
-import * as net from 'net';
-
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class PortService {
-  checkPort(port: number): Promise<boolean> {
-    return new Promise((resolve, reject) => {
-      const server = net.createServer();
-
-      server.once('error', (err: any) => {
-        if (err.code === 'EADDRINUSE') {
-          resolve(true); // 端口被占用
-        } else {
-          reject(err);
-        }
-      });
-
-      server.once('listening', () => {
-        server.close();
-        resolve(false); // 端口未被占用
-      });
-
-      server.listen(port);
-    });
-  }
-
-  async findAvailablePort(startPort: number): Promise<number> {
-    let port = startPort;
-    while (await this.checkPort(port)) {
-      port += 1;
-    }
-    return port;
-  }
-}
-
-export default async function bootstrap() {
-  console.log('初始化');
-
-  const portUtils = new PortService();
-
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  console.log('初始化完成');
-  app.enableCors();
-  app.useStaticAssets('public');
-  // 这里很重要，vercel是没有状态，需要用session建立一个状态，不然vercel会报500
-  app.use(
-    session({
-      secret: 'blog-server-secret',
-      rolling: true,
-      name: 'blog-server',
-      cookie: { maxAge: null },
-    }),
-  );
-  console.log('服务挂载');
-  const port = await portUtils.findAvailablePort(3000);
-  await app.listen(port);
-  console.log('服务完成');
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const host = '0.0.0.0'; // 监听所有网络接口
+  await app.listen(3005, host);
 }
 bootstrap();
