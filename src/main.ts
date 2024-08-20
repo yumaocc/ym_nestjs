6afe20f5b0cc038d27f5d684f14bd6b5e6382c2e
hongguoqiang <2310087518@qq.com>
2024-08-20 10:42:07 +0800

    feat: 设置监听ip

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(3005);
+  const host = '0.0.0.0'; // 监听所有网络接口
+  await app.listen(3005, host);
 }
 bootstrap();
